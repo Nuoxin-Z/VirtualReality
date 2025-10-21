0cef39419e1098b5e65e37243fadcaad6a45dbd9
Nuoxin-Z <nuoxinz@nycstudents.net>
2025-10-21 14:58:21 +0000

    10/21/25 10:57 a.m.

--- VR_Unit2/VRPractical2_2/script.js
+++ VR_Unit2/VRPractical2_2/script.js
@@ -1,5 +1,30 @@
 
 window.addEventListener("DOMContentLoaded",function() {
 
-    
+    loop()    
 })
+
+function loop(){
+    //Make the car move to the left.
+    car = document.querySelector("#car");
+    car.setAttribute("position",{x:car.getAttribute("position").x+=0.1, y:0,z:-8});
+    
+
+    //Make the pokeball rotate on the x axis
+    pokemonball = document.querySelector("#pokemonball");
+    pokemonball.setAttribute("rotation",{x:pokemonball.getAttribute("rotation").x+=1, y:0,z:0});
+
+
+    //Make the rocket fly up.
+    rocket = document.querySelector("#rocket");
+    rocket.setAttribute("position",{y:rocket.getAttribute("position").y+=0.1, x:0,z:0});
+
+    //Make the dude “grow”
+    dude = document.querySelector("#dude");
+    dude.setAttribute("scale",{z:dude}.getAttribute("position").y+=0.1, x:0,z:0});
+
+    //Make the sun fade in
+
+    window.requestAnimationFrame(loop);
+
+}
