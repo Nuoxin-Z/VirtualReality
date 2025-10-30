b1ce7861584e1d7d74792e2bbfc9ae7148e0edd2
Nuoxin-Z <nuoxinz@nycstudents.net>
2025-10-30 04:41:02 +0000

    10/30/25 00:40 a.m.

--- VR_Unit2/VRPractical2_4/rocket.js
+++ VR_Unit2/VRPractical2_4/rocket.js
@@ -1,12 +1,59 @@
-window.addEventListener("DOMContentLoaded",function() {
+class rocket{
+  constructor(x,y,z){
+    this.obj = document.createElement("a-entity");
 
-    loop()    
-})
+    let head = document.createElement("a-cone");
+    head.setAttribute("color","blue");
+    head.setAttribute("position","0 0 -2");
+    head.setAttribute("height","1");
+    head.setAttribute("radius-top","0");
+    head.setAttribute("radius-bottom","0.5");
+    this.obj.appendChild( head );
+  
+    let body = document.createElement("a-cylinder");
+    body.setAttribute("position","0 -1.5 -2");
+    body.setAttribute("color","red");
+    body.setAttribute("radius","0.5");
+    body.setAttribute("height","2");
+    this.obj.appendChild( body );
+
+    let bottom = document.createElement("a-cone");
+    bottom.setAttribute("color","orange");
+    bottom.setAttribute("position","0 -3.5 -2");
+    bottom.setAttribute("height","2");
+    bottom.setAttribute("radius-top","0");
+    bottom.setAttribute("radius-bottom","0.25");
+    bottom.setAttribute("rotation","-180 0 0");
+    this.obj.appendChild( bottom );
+  
 
 
-function loop(){
-    rocket = document.querySelector("#rocket");
-    rocket.setAttribute("position",{y:rocket.getAttribute("position").y+=0.1, x:0,z:0});
-    window.requestAnimationFrame(loop);
+  
+    this.obj.setAttribute("position",{x:x, y:y, z:z});
+    scene.append( this.obj )
 
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.obj.setAttribute("position",{x:this.x, y:this.y, z:this.z});
+
+    this.speed = rnd(10,50)/1000;
+    scene.appendChild(this.obj);
+  }
+  
+  launch(){
+    if(this.speed > 0){
+      this.y += this.speed;
+      if (this.y > 10){
+        this.speed = -this.speed;
+        }
+      }else if (this.y < 0){  
+        this.y = this.speed;
+        if(this.y<=0){
+          this.y = 0;
+          this.speed = 0;
+        }
+    }
+      this.obj.setAttribute("position",{x:this.x, y:this.y, z:this.z});
+  }
 }
--- VR_Unit2/VRPractical2_4/script.js
+++ VR_Unit2/VRPractical2_4/script.js
@@ -1,15 +1,42 @@
 let rnd = (l,u) => Math.floor(Math.random()*(u-l) + l);
 let scene;
+let rockets = [];
 
 window.addEventListener("DOMContentLoaded",function() {
   scene = document.querySelector("a-scene"); 
   
 
+  let grid
+
+
+
+//Challenge 3
+//1) Create 100 rockets in a grid pattern on the X-Z plane just below the plane.  Provide each rocket an appropriate random y coordinate below the plane.
+//2) Animate each rocket “launching”. 
+
+for(let i=0; i<10; i++){
+  for(let j=0; j<10; j++){
+    let rx = i - 5;
+    let rz = j - 5;
+    let ry = rnd(-5,0);
+    let r = new rocket(rx, ry, rz);
+    rockets.push(r);
+  }
+}
+
+
+
   loop();
 })
 
 function loop(){
-
+  for (let i=0; i<rockets.length; i++){
+    let rocket = rockets[i];
+  if(rocket.y<0 && rnd(1,100)===1){
+    rocket.speed = rnd(10,50)/1000;
+  }
+    rockets.launch();
+  }
   
   window.requestAnimationFrame( loop );
 }
