b75050b5b5afc4828c8e77cdfa0a5f0793a66fed
Nuoxin-Z <nuoxinz@nycstudents.net>
2025-11-21 14:36:13 +0000

    9:35 a.m. 11/21/25

--- VR_Unit3/VRPractical3_2/script.js
+++ VR_Unit3/VRPractical3_2/script.js
@@ -1,16 +1,29 @@
-let scene;
+let scene,car;
 
 
 window.addEventListener("DOMContentLoaded",function() {
   scene = document.querySelector("a-scene")
 
-
+  car = document.getElementById("car");
+  car.r = 0;
+  car.dr = 1;
+//Make the car from Practical 1.1 drive when use clicks on the car.
+  car.addEventListener("mouseenter",function(){
+    car.rotate =true;
+  });
+  car.addEventListener("mouseleave",function(){
+    car.rotate =false;
+  });
+ 
       
   loop();
 })
 
 function loop(){
-
+  if(car.rotate){
+    car.r += car.dr;
+    car.setAttribute("rotation",{x:car.r, y:0, z:0});
+  };
     
   window.requestAnimationFrame(loop);
 }
